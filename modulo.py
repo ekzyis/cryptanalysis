b6df6cc3c1077ab4d3eb7890683133f030f9b525
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-01 20:56:09 +0200

    Trim trailing whitespace

--- modulo.py
+++ modulo.py
@@ -6,7 +6,7 @@ class TestModuloVsBitShift(unittest.TestCase):
   def test_modulo_and_bitshift_are_not_commutative(self):
     m = 256 # modulo
     b = 2   # bit shift
-    a = 0   
+    a = 0
     self.assertEqual( (a % m) >> b, (a >> b) % m)
     a = 256
     # 256 % 256 = 0, 0 >> 2 = 0 vs. 256 >> 2 = 64, 64 % 256 = 64
