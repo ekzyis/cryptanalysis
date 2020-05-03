3649e6b239bd87cc9a5624e1e357c623f3cfe6cb
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-03 23:12:57 +0200

    Abbreviate Differential Cryptoanalysis with DCA

--- src/dca/dca.py
+++ src/dca/dca.py
@@ -8,7 +8,7 @@ class TestDCA(unittest.TestCase):
 
     def test_that_there_are_2_to_the_power_of_n_amount_of_pairs_for_each_xor_value(self):
         """
-        Assert that the following sentence in C. Bender's Master Thesis about Differential Cryptoanalysis
+        Assert that the following sentence in C. Bender's Master Thesis about DCA
         in 3.1.2 Substitution is correct (and correctly understood):
         "Jede Eingabedifferenz ∆d kann von 2**n verschiedenen Eingabepaaren (m1, m2) mit m1 ⊕ m2 = ∆d erzeugt werden."
         """
