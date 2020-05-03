1e520e0a28e9954f066e226772abc55d7a173638
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-03 16:32:07 +0200

    Formatting

--- src/feal.py
+++ src/feal.py
@@ -2,16 +2,19 @@
 
 import unittest
 
-def s0(a,b):
-  return (a + b % 256) >> 2
+
+def s0(a, b):
+    return (a + b % 256) >> 2
+
 
 class TestS0(unittest.TestCase):
-  def test_case_1(self):
-    a = 0b10101010
-    b = 0b01010101
-    s = s0(a,b)
-    self.assertEqual(0b111111, s)
-    self.assertEqual(63, s)
+    def test_case_1(self):
+        a = 0b10101010
+        b = 0b01010101
+        s = s0(a, b)
+        self.assertEqual(0b111111, s)
+        self.assertEqual(63, s)
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
--- src/modulo.py
+++ src/modulo.py
@@ -2,25 +2,26 @@
 
 import unittest
 
-class TestModuloVsBitShift(unittest.TestCase):
-  def test_modulo_and_bitshift_are_not_commutative(self):
-    """
-    This test case "proves" that the commutative law does not hold "%" and ">>"
-    """
-    m = 256 # modulo
-    b = 2   # bit shift
-    a = 0
-    self.assertEqual( (a % m) >> b, (a >> b) % m)
-    a = 256
-    # 256 % 256 = 0, 0 >> 2 = 0 vs. 256 >> 2 = 64, 64 % 256 = 64
-    self.assertNotEqual( (a % m) >> b, (a >> b) % m)
 
-  def test_modulo_with_power_of_two_can_be_rewritten_with_bitwise_and(self):
-    for n in range(10):
-      for x in range(1024):
-        e = 2**n
-        self.assertEqual(x % e, x & (e-1))
+class TestModuloVsBitShift(unittest.TestCase):
+    def test_modulo_and_bitshift_are_not_commutative(self):
+        """
+        This test case "proves" that the commutative law does not hold "%" and ">>"
+        """
+        m = 256  # modulo
+        b = 2  # bit shift
+        a = 0
+        self.assertEqual((a % m) >> b, (a >> b) % m)
+        a = 256
+        # 256 % 256 = 0, 0 >> 2 = 0 vs. 256 >> 2 = 64, 64 % 256 = 64
+        self.assertNotEqual((a % m) >> b, (a >> b) % m)
+
+    def test_modulo_with_power_of_two_can_be_rewritten_with_bitwise_and(self):
+        for n in range(10):
+            for x in range(1024):
+                e = 2 ** n
+                self.assertEqual(x % e, x & (e - 1))
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
