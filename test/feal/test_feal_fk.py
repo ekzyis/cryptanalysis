7312e970360d188cd2211c8ee9637f4999b1fb04
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-05 23:02:07 +0200

    (TDD) Added test case for fk function

---
+++ test/feal/test_feal_fk.py
@@ -0,0 +1,15 @@
+import unittest
+
+# noinspection PyUnresolvedReferences
+import test.context
+from ciphers.feal import fk
+
+
+class TestFealFk(unittest.TestCase):
+
+    def test_fk_returns_expected_output_as_specificed_in_reference(self):
+        """In section 5.2 of https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf,
+        there are example inputs with the expected output.
+        """
+        f = fk(0x00000000, 0x00000000)
+        self.assertEqual(f, 0x10041044)
