276b83a34c8643ef5004f19c6243a9983eb2e6d6
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-06 01:00:18 +0200

    Changed specification of rotate function
    Rotation must be given max_bit of output to ensure correct wrapping at for example 8-bit for all numbers.

--- src/util/rot.py
+++ src/util/rot.py
@@ -1,18 +1,18 @@
-def rot_left(bits, i):
+def rot_left(bits, i, max_bit):
     """Bit rotation / Circular shift to the left.
-    Shifts i bits to the left and adds the bits that fall of to the right side."""
-    max_bit = 128
-    while 2 ** max_bit > bits:
-        max_bit -= 1
-    max_bit += 1
+    Shifts i bits to the left and adds the bits that fall of to the right side.
+    max_bit defines the "bit border" after which bits are wrapped around.
+    Raises ValueError when max_bit is smaller than the highest bit in bits."""
+    if bits > 2**max_bit:
+        raise ValueError("max_bit is smaller than highest bit in bits")
     return ((bits % 2 ** (max_bit - i)) << i) | (bits >> (max_bit - i))
 
 
-def rot_right(bits, i):
+def rot_right(bits, i, max_bit):
     """Bit rotation / Circular shift to the right.
-    Shifts i bits to the right and adds the bits that fall of to the left side."""
-    max_bit = 128
-    while 2 ** max_bit > bits:
-        max_bit -= 1
-    max_bit += 1
+    Shifts i bits to the right and adds the bits that fall of to the left side.
+    max_bit defines the bit at which bits from the right appear at the left.
+    Raises ValueError when max_bit is smaller than the highest bit in bits."""
+    if bits > 2**max_bit:
+        raise ValueError("max_bit is smaller than highest bit in bits")
     return (bits >> i) | (bits % 2 ** i) << (max_bit - i)
--- test/util/test_rot.py
+++ test/util/test_rot.py
@@ -8,14 +8,15 @@ from util.rot import rot_left, rot_right
 class TestRot(unittest.TestCase):
 
     def test_rot_left_adds_left_shifted_bits_to_right_side(self):
-        self.assertEqual(rot_left(0b11001, 2), 0b00111)
-        self.assertEqual(rot_left(0b1001110, 2), 0b0111010)
-        # ignores leading zeroes; overflow happens right after highest set bit
-        self.assertEqual(rot_left(0b001001110, 2), 0b0111010)
-        self.assertEqual(rot_left(0b10110011, 4), 0b00111011)
+        # max_bit equal to highest bit which leads to wrap-around
+        self.assertEqual(rot_left(0b1101, 2, 4), 0b0111)
+        # max_bit higher than highest bit with no wrap-around
+        self.assertEqual(rot_left(0b001101, 2, 6), 0b110100)
+        # max_bit higher than highest bit with wrap-around
+        self.assertEqual(rot_left(0b001101, 4, 6), 0b010011)
 
     def test_rot_right_adds_right_shifted_bits_to_left_side(self):
-        self.assertEqual(rot_right(0b11001, 2), 0b1110)
-        self.assertEqual(rot_right(0b1001110, 2), 0b1010011)
-        self.assertEqual(rot_right(0b001001110, 2), 0b1010011)
-        self.assertEqual(rot_right(0b10110011, 4), 0b00111011)
+        # max_bit equal to highest bit with regular wrap-around
+        self.assertEqual(rot_right(0b1110, 2, 4), 0b1011)
+        # max_bit higher than highest bit which leads to unregular wrap-around
+        self.assertEqual(rot_right(0b1101, 2, 8), 0b01000011)
