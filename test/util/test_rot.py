70c2a7650afd1b698b70d05599ebbae8f89eacf2
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-05 23:49:30 +0200

    Fix test

--- test/util/test_rot.py
+++ test/util/test_rot.py
@@ -9,7 +9,7 @@ class TestRot(unittest.TestCase):
 
     def test_rot_left_adds_left_shifted_bits_to_right_side(self):
         self.assertEqual(rot_left(0b11001, 2), 0b00111)
-        self.assertEqual(rot_left(0b1001110, 2), 0b1101001)
+        self.assertEqual(rot_left(0b1001110, 2), 0b0111010)
 
     def test_rot_right_adds_right_shifted_bits_to_left_side(self):
         self.assertEqual(rot_right(0b11001, 2), 0b1110)
