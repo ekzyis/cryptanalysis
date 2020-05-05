f55531b0e53094c911182982b28e1371a9009e90
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-06 01:19:38 +0200

    (TDD) Added test case for FEAL f function

---
+++ test/feal/test_feal_f.py
@@ -0,0 +1,21 @@
+import unittest
+
+# noinspection PyUnresolvedReferences
+import test.context
+from ciphers.feal import f
+
+
+class TestFEALCipherF(unittest.TestCase):
+
+    def test_f_returns_expected_output_as_specified_in_reference(self):
+        """In section 5.1 of https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf,
+        there are example inputs with the expected output.
+        """
+        _f = f(0x00FFFF00, 0xFFFF)
+        self.assertEqual(_f, 0x10041044)
+
+    def test_f_raises_value_error_when_input_keys_are_not_32_bit(self):
+        with self.assertRaises(ValueError):
+            f(0xF000F000F, 0x0000000)
+        with self.assertRaises(ValueError):
+            f(0x00000000, 0xF000F000F)
