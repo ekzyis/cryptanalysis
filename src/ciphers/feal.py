124f49d5a3644a19dca564a6ad524fa490e3cc0c
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-05 21:12:00 +0200

    Added comment about bit order

--- src/ciphers/feal.py
+++ src/ciphers/feal.py
@@ -31,7 +31,7 @@ from docopt import docopt
 
 
 def split(n, size, bits):
-    """Splits the input bits into n bitstrings with given size.
+    """Splits the input bits into n bitstrings with given size. The highest bits are returned first.
     Raises error when n is equal to 0 or 1 or concatenation of bitstring is smaller than input bits."""
     if n == 0 or n == 1:
         raise ValueError("n can not be 0 or 1")
