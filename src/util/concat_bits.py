83130c4ce1eb5be4ec7025458d963690abb6287f
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-06 11:54:01 +0200

    Implemented concat_bits fn

--- src/util/concat_bits.py
+++ src/util/concat_bits.py
@@ -2,4 +2,11 @@ def concat_bits(*args, n):
     """Concatenates the given subkeys into one key.
     The first key is shifted to the highest n-bits, the second key to the second highest n-bits and so forth.
     Raises error when one subkey is larger than n."""
-    pass
+    r = 0
+    args = list(args)  # convert args to list because *args creates tuple
+    args.reverse()
+    for i, arg in enumerate(args):
+        if arg > 2 ** n:
+            raise ValueError("subkey is larger than slot given by n")
+        r |= arg << (i*n)
+    return r
