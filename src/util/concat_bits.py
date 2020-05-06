a29c71039e198a95b1982f574e6d0ed6bceacf97
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-06 11:42:40 +0200

    (TDD) Added test case for bit concatenation fn

---
+++ src/util/concat_bits.py
@@ -0,0 +1,5 @@
+def concat_bits(*args, n):
+    """Concatenates the given subkeys into one key.
+    The first key is shifted to the highest n-bits, the second key to the second highest n-bits and so forth.
+    Raises error when one subkey is larger than n."""
+    pass
---
+++ test/util/test_concat_bits.py
@@ -0,0 +1,28 @@
+import unittest
+
+# noinspection PyUnresolvedReferences
+import test.context
+from util.concat_bits import concat_bits
+
+
+class TestCombine(unittest.TestCase):
+
+    def test_concat_bits_concatenates_four_8_bit_strings_into_one_32_bit_string(self):
+        k0 = 0xAF
+        k1 = 0x13
+        k2 = 0xD3
+        k3 = 0x8C
+        out = concat_bits(k0, k1, k2, k3, n=8)
+        self.assertEqual(out, 0xAF13D38C)
+
+    def test_concat_bits_concatenates_8_four_bit_strings_into_one_32_bit_string(self):
+        k0 = 0xA
+        k1 = 0xE
+        k2 = 0x9
+        k3 = 0x1
+        k4 = 0x8
+        k5 = 0xA
+        k6 = 0xB
+        k7 = 0x5
+        out = concat_bits(k0, k1, k2, k3, k4, k5, k6, k7, n=4)
+        self.assertEqual(out, 0xAE918AB5)
