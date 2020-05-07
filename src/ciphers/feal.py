4dc4776cc5d4f8177eb5149d31aaf0b94209bf81
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-07 17:06:02 +0200

    Better ordering of quotes in desc

--- src/ciphers/feal.py
+++ src/ciphers/feal.py
@@ -3,16 +3,16 @@
 """
 FEAL-NX Implementation.
 
+The specification found at https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf is used as a reference
+for this implementation.
+
+It follows a short description of the cipher and its implementation:
+
     "In cryptography, FEAL (the Fast data Encipherment ALgorithm) is a block cipher proposed as an alternative
     to the Data Encryption Standard (DES), and designed to be much faster in software. The Feistel based algorithm
     was first published in 1987 by Akihiro Shimizu and Shoji Miyaguchi from NTT. The cipher is susceptible to various
     forms of cryptanalysis, and has acted as a catalyst in the discovery of differential and linear cryptanalysis."
     - Wikipedia, https://en.wikipedia.org/wiki/FEAL
-
-The specification found at https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf is used as a reference
-for this implementation.
-
-It follows a short description of the cipher and its implementation:
 
     "FEAL, the Fast Data Encipherment Algorithm, is a 64-bit block cipher algorithm that enciphers 64-bit plaintexts
     into 64-bit ciphertexts and vice versa. FEAL has three options: key length, round number and key parity.
