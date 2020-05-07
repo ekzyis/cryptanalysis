b83dd566272cf6ebef938e344f381fe1ea1b1cc4
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-07 15:50:11 +0200

    Consistent test ordering (just as in test_encrypt)

--- test/feal/test_feal_decrypt.py
+++ test/feal/test_feal_decrypt.py
@@ -7,17 +7,6 @@ from ciphers.feal import decrypt, decrypt_preprocessing, decrypt_iterative_calcu
 
 class TestFEALCipherDecrypt(unittest.TestCase):
 
-    def test_decrypt_matches_specification_in_paper(self):
-        """Checks that the FEAL decryption decrypts the given ciphertext."""
-        # i/o values taken from test for encrypt.
-        #   Since decryption should reverse encryption, I assume I can
-        #   just pass in the output of encrypt into the input of decrypt
-        #   and expect the input of encrypt as output of decrypt.
-        c = 0x9C9B54973DF685F8
-        k = 0x123456789ABCDEF0123456789ABCDEF
-        p = decrypt(c, k)
-        self.assertEqual(p, 0x0)
-
     def test_decrypt_raises_value_error_if_text_larger_than_64_bit(self):
         with self.assertRaises(ValueError):
             decrypt(2 ** 64, 0x0)
@@ -33,6 +22,17 @@ class TestFEALCipherDecrypt(unittest.TestCase):
             decrypt(0x0, 2 ** 128 - 1)
         except ValueError:
             self.fail("decrypt raised unexpected ValueError")
+
+    def test_decrypt_matches_specification_in_paper(self):
+        """Checks that the FEAL decryption decrypts the given ciphertext."""
+        # i/o values taken from test for encrypt.
+        #   Since decryption should reverse encryption, I assume I can
+        #   just pass in the output of encrypt into the input of decrypt
+        #   and expect the input of encrypt as output of decrypt.
+        c = 0x9C9B54973DF685F8
+        k = 0x123456789ABCDEF0123456789ABCDEF
+        p = decrypt(c, k)
+        self.assertEqual(p, 0x0)
 
     def test_decrypt_preprocessing_matches_specification_in_paper(self):
         # i/o values taken from test for encrypt.
