68129c8edc0c7197cf3c8a8f981428431ab7d00f
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 12:33:37 +0200

    Implemented ECB modi

--- src/ciphers/modi/ecb.py
+++ src/ciphers/modi/ecb.py
@@ -1,2 +1,20 @@
+from math import ceil
+from util.split import split
+from util.concat_bits import concat_bits
+
+
 def ecb(blocksize):
-    pass
+    """Implements the Electronic Code Book Mode (ECB) for ciphers.
+    Returns a function which needs the cipher function and the argument for it
+    as arguments.
+    Works by splitting the argument into the needed blocks, passing those blocks to the
+    cipher function and then return the concatenation of the results.
+    """
+
+    def _ecb(cipher_fn, arg, arg_bits):
+        n = ceil(arg_bits / blocksize)
+        in_blocks = split(n, blocksize, arg)
+        out_blocks = [cipher_fn(b) for b in in_blocks]
+        return concat_bits(*out_blocks, n=blocksize)
+
+    return _ecb
