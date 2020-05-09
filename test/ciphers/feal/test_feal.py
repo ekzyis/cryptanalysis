99d5b62dddae93c0e67e033cc9ead10dca848af8
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 15:36:23 +0200

    Testing format with encrypt and decrypt now

--- test/ciphers/feal/test_feal.py
+++ test/ciphers/feal/test_feal.py
@@ -36,22 +36,62 @@ class TestFeal(unittest.TestCase):
         p = feal()
         self.assertEqual(int(p), 0x0)
 
-    @default_decrypt_args('-o', 'bin')
     def test_integration_feal_output_format_bin(self):
-        p = feal()
-        self.assertEqual(p, '0b0')
+        @default_encrypt_args('-o', 'bin')
+        def test_format_bin_with_encrypt():
+            c = feal()
+            self.assertEqual(c, '0b1001110010011011010101001001011100111101111101101000010111111000')
 
-    @default_decrypt_args('-o', 'oct')
+        test_format_bin_with_encrypt()
+
+        @default_decrypt_args('-o', 'bin')
+        def test_format_bin_with_decrypt():
+            p = feal()
+            self.assertEqual(p, '0b0')
+
+        test_format_bin_with_decrypt()
+
     def test_integration_feal_output_format_oct(self):
-        p = feal()
-        self.assertEqual(p, '0o0')
+        @default_encrypt_args('-o', 'oct')
+        def test_format_oct_with_encrypt():
+            c = feal()
+            self.assertEqual(c, '0o1162332511347575502770')
 
-    @default_decrypt_args('-o', 'dec')
+        test_format_oct_with_encrypt()
+
+        @default_decrypt_args('-o', 'oct')
+        def test_format_oct_with_decrypt():
+            p = feal()
+            self.assertEqual(p, '0o0')
+
+        test_format_oct_with_decrypt()
+
     def test_integration_feal_output_format_dec(self):
-        p = feal()
-        self.assertEqual(p, '0')
+        @default_encrypt_args('-o', 'dec')
+        def test_format_dec_with_encrypt():
+            c = feal()
+            self.assertEqual(c, '11284706299863270904')
 
-    @default_decrypt_args('-o', 'hex')
+        test_format_dec_with_encrypt()
+
+        @default_decrypt_args('-o', 'dec')
+        def test_format_dec_with_decrypt():
+            p = feal()
+            self.assertEqual(p, '0')
+
+        test_format_dec_with_decrypt()
+
     def test_integration_feal_output_format_hex(self):
-        p = feal()
-        self.assertEqual(p, '0x0')
+        @default_encrypt_args('-o', 'hex')
+        def test_format_hex_with_encrypt():
+            c = feal()
+            self.assertEqual(c, '0x9c9b54973df685f8')
+
+        test_format_hex_with_encrypt()
+
+        @default_decrypt_args('-o', 'hex')
+        def test_format_hex_with_decrypt():
+            p = feal()
+            self.assertEqual(p, '0x0')
+
+        test_format_hex_with_decrypt()
