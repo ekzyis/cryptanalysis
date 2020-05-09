39fa7e684e9866b72b59bd884388a3dd1e362d0e
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 14:45:08 +0200

    Renamed function

--- test/ciphers/feal/test_feal.py
+++ test/ciphers/feal/test_feal.py
@@ -6,7 +6,7 @@ import test.context
 from ciphers.feal import feal
 
 
-def mock_patch_wrapper(*default_args):
+def patch_sysv_wrapper(*default_args):
     def additional_args_wrapper(*add_args):
         def test_wrapper(fn):  # wraps the test function
             @mock.patch('sys.argv', default_args + add_args)
@@ -20,9 +20,9 @@ def mock_patch_wrapper(*default_args):
     return additional_args_wrapper
 
 
-default_encrypt_args = mock_patch_wrapper('feal', 'encrypt', '0x123456789ABCDEF0123456789ABCDEF', '0')
+default_encrypt_args = patch_sysv_wrapper('feal', 'encrypt', '0x123456789ABCDEF0123456789ABCDEF', '0')
 
-default_decrypt_args = mock_patch_wrapper('feal', 'decrypt', '0x123456789ABCDEF0123456789ABCDEF', '0x9C9B54973DF685F8')
+default_decrypt_args = patch_sysv_wrapper('feal', 'decrypt', '0x123456789ABCDEF0123456789ABCDEF', '0x9C9B54973DF685F8')
 
 
 class TestFeal(unittest.TestCase):
