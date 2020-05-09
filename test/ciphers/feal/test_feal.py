be21e671440e4dd485d990e7934fc962e42d0c2b
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 16:02:51 +0200

    Added integration test for round number option

--- test/ciphers/feal/test_feal.py
+++ test/ciphers/feal/test_feal.py
@@ -99,3 +99,18 @@ class TestFeal(unittest.TestCase):
             self.assertEqual(p, '0x0')
 
         test_format_hex_with_decrypt()
+
+    def test_integration_feal_option_round_number(self):
+        @default_encrypt_args('-n', '16')
+        def test_round_number_with_encrypt():
+            c = feal()
+            self.assertEqual(int(c), 0x1A94383EB19BA07)
+
+        test_round_number_with_encrypt()
+
+        @default_decrypt_args('-n', '16', text='0x1A94383EB19BA07')
+        def test_round_number_with_decrypt():
+            p = feal()
+            self.assertEqual(int(p), 0x0)
+
+        test_round_number_with_decrypt()
