670189001f0eff60c860920d312b22668aed7984
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 19:18:23 +0200

    Fixed calculation of needed bits

--- src/ciphers/modi/ecb.py
+++ src/ciphers/modi/ecb.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import log2, ceil
 
 from util.concat_bits import concat_bits
 from util.split import split
@@ -14,10 +14,12 @@ def ecb(blocksize):
     :param blocksize        Block size of cipher in bits on which we want to use ECB mode
     """
 
-    def _ecb(cipher_fn, key, text, arg_bits):
-        n = ceil(arg_bits / blocksize)
+    def _ecb(cipher_fn, key, text, *args, **kwargs):
+        # calculate amount of blocks
+        min_text_bits = ceil(log2(text + 1))
+        n = ceil(min_text_bits / blocksize)
         in_blocks = split(n, blocksize, text)
-        out_blocks = [cipher_fn(key, b) for b in in_blocks]
+        out_blocks = [cipher_fn(key, b, *args, **kwargs) for b in in_blocks]
         return concat_bits(*out_blocks, n=blocksize)
 
     return _ecb
