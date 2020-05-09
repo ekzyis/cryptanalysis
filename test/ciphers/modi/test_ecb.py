76de272783adb77ce08bf3a281b8ac1a0cb76ec6
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-09 12:39:02 +0200

    Using setUp function

--- test/ciphers/modi/test_ecb.py
+++ test/ciphers/modi/test_ecb.py
@@ -8,13 +8,13 @@ from ciphers.modi.ecb import ecb
 
 
 class TestECB(unittest.TestCase):
+
+    def setUp(self):
+        self.cipher_fn = lambda m: m >> 4
+
     def test_ecb_returns_concatenation_of_cipher_fn_results(self):
         m = 0x1234ABCD5678
-
-        def encrypt(m):
-            return m >> 4
-
-        cipher_fn = mock.Mock(wraps=encrypt)
+        cipher_fn = mock.Mock(wraps=self.cipher_fn)
         c = ecb(blocksize=16)(cipher_fn, m, 3 * 16)
         self.assertEqual(cipher_fn.call_count, 3)
         cipher_fn.assert_has_calls([call(0x1234), call(0xABCD), call(0x5678)])
