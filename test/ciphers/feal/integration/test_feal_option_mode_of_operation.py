d096a5f50ca0f0f995697676bca3f01a6bc32999
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 14:12:02 +0200

    Rename test cases

--- test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
+++ test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
@@ -10,35 +10,35 @@ class TestFeal(unittest.TestCase):
 
     def test_integration_feal_option_mode_of_operation_ecb_with_no_remainder(self):
         @default_encrypt_args('-m', 'ecb', key='0xffff', text='0xfffafffafffafffafffafffafffafffa')
-        def test_mode_of_operation_ecb_with_encrypt():
+        def test_mode_of_operation_ecb_with_encrypt_no_remainder():
             c = feal()
             #              KEY    TEXT                  OUTPUT IN HEX
             # feal encrypt 0xffff 0xfffafffafffafffa    0xa281baaa4303e813
             self.assertEqual(hex(int(c, 0)).lower(), '0xa281baaa4303e813a281baaa4303e813')
 
-        test_mode_of_operation_ecb_with_encrypt()
+        test_mode_of_operation_ecb_with_encrypt_no_remainder()
 
         @default_decrypt_args('-m', 'ecb', key='0xffff', text='0xa281baaa4303e813a281baaa4303e813')
-        def test_mode_of_operation_ecb_with_decrypt():
+        def test_mode_of_operation_ecb_with_decrypt_no_remainder():
             p = feal()
             self.assertEqual(hex(int(p, 0)).lower(), '0xfffafffafffafffafffafffafffafffa')
 
-        test_mode_of_operation_ecb_with_decrypt()
+        test_mode_of_operation_ecb_with_decrypt_no_remainder()
 
     def test_integration_feal_option_mode_of_operation_ecb_with_remainder(self):
         @default_encrypt_args('-m', 'ecb', key='0xffff', text='0xfffafffafffafffafffafffa')
-        def test_mode_of_operation_ecb_with_encrypt():
+        def test_mode_of_operation_ecb_with_encrypt_remainder():
             c = feal()
             #              KEY    TEXT                  OUTPUT IN HEX
             # feal encrypt 0xffff 0xfffafffa            0x1ab00c4bbbc208e6
             # feal encrypt 0xffff 0xfffafffafffafffa    0xa281baaa4303e813
             self.assertEqual(hex(int(c, 0)).lower(), '0x1ab00c4bbbc208e6a281baaa4303e813')
 
-        test_mode_of_operation_ecb_with_encrypt()
+        test_mode_of_operation_ecb_with_encrypt_remainder()
 
         @default_decrypt_args('-m', 'ecb', key='0xffff', text='0x1ab00c4bbbc208e6a281baaa4303e813')
-        def test_mode_of_operation_ecb_with_decrypt():
+        def test_mode_of_operation_ecb_with_decrypt_remainder():
             p = feal()
             self.assertEqual(hex(int(p, 0)).lower(), '0xfffafffafffafffafffafffa')
 
-        test_mode_of_operation_ecb_with_decrypt()
+        test_mode_of_operation_ecb_with_decrypt_remainder()
