f465ebd284ffcce5bdaa2ce2fb6c63aedb2f74a4
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 20:02:27 +0200

    Added -x as encoding option to usage

--- src/ciphers/feal.py
+++ src/ciphers/feal.py
@@ -28,6 +28,7 @@ Usage:
     -n=N, --round-number=N  Number of rounds. Must be even. [default: 32]
     -o=[bin,hex,oct,dec]    Specifies the output format. [default: dec]
     -m=[ecb,none]           Specifies the mode of operation [default: none]
+    -x=[utf8,none]          Specifies the encoding of the cipher-/plaintext. [default: utf8]
 
     KEY                     The key which should be used for en-/decryption.
     PLAINTEXT               The text to encrypt. Must be a number. Can be a code literal such as 0b1011, 0o71, 0xF32C.
