a786fa1f5d5f17aa0d708515928292fdd89d5ecf
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 13:57:55 +0200

    Fix bad converting from FEAL output to integer
    Output was in decimal but was interpreted as hex.

--- test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
+++ test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
@@ -14,13 +14,13 @@ class TestFeal(unittest.TestCase):
             c = feal()
             #              KEY    TEXT                 OUTPUT IN HEX
             # feal encrypt 0xFFFF 0xFFFAFFFAFFFAFFFA   0xa281baaa4303e813
-            self.assertEqual(hex(int(c, 16)), '0xa281baaa4303e813a281baaa4303e813')
+            self.assertEqual(hex(int(c, 0)), '0xa281baaa4303e813a281baaa4303e813')
 
         test_mode_of_operation_ecb_with_encrypt()
 
         @default_decrypt_args('-m', 'ecb', key='0xFFFF', text='0xa281baaa4303e813a281baaa4303e813')
         def test_mode_of_operation_ecb_with_decrypt():
             p = feal()
-            self.assertEqual(hex(int(p, 16)), '0xFFFAFFFAFFFAFFFAFFFAFFFAFFFAFFFA')
+            self.assertEqual(hex(int(p, 0)), '0xFFFAFFFAFFFAFFFAFFFAFFFAFFFAFFFA')
 
         test_mode_of_operation_ecb_with_decrypt()
