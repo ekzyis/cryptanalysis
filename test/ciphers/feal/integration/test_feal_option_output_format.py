c015d2956686770d2f00687257488da9c9c7a486
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 14:37:23 +0200

    Remove _option from test case names in integration tests for output format

--- test/ciphers/feal/integration/test_feal_option_output_format.py
+++ test/ciphers/feal/integration/test_feal_option_output_format.py
@@ -8,41 +8,41 @@ from test.ciphers.feal.integration.wrappers import default_decrypt_args, default
 
 class TestFealOptionOutputFormat(unittest.TestCase):
     @default_encrypt_args('-o', 'bin')
-    def test_integration_feal_encrypt_option_output_format_bin(self):
+    def test_integration_feal_encrypt_output_format_bin(self):
         c = feal()
         self.assertEqual(c, '0b1001110010011011010101001001011100111101111101101000010111111000')
 
     @default_decrypt_args('-o', 'bin')
-    def test_integration_feal_decrypt_option_output_format_bin(self):
+    def test_integration_feal_decrypt_output_format_bin(self):
         p = feal()
         self.assertEqual(p, '0b0')
 
     @default_encrypt_args('-o', 'oct')
-    def test_integration_feal_encrypt_option_output_format_oct(self):
+    def test_integration_feal_encrypt_output_format_oct(self):
         c = feal()
         self.assertEqual(c, '0o1162332511347575502770')
 
     @default_decrypt_args('-o', 'oct')
-    def test_integration_feal_decrypt_option_output_format_oct(self):
+    def test_integration_feal_decrypt_output_format_oct(self):
         p = feal()
         self.assertEqual(p, '0o0')
 
     @default_encrypt_args('-o', 'dec')
-    def test_integration_feal_encrypt_option_output_format_dec(self):
+    def test_integration_feal_encrypt_output_format_dec(self):
         c = feal()
         self.assertEqual(c, '11284706299863270904')
 
     @default_decrypt_args('-o', 'dec')
-    def test_integration_feal_decrypt_option_output_format_dec(self):
+    def test_integration_feal_decrypt_output_format_dec(self):
         p = feal()
         self.assertEqual(p, '0')
 
     @default_encrypt_args('-o', 'hex')
-    def test_integration_feal_encrypt_option_output_format_hex(self):
+    def test_integration_feal_encrypt_output_format_hex(self):
         c = feal()
         self.assertEqual(c, '0x9c9b54973df685f8')
 
     @default_decrypt_args('-o', 'hex')
-    def test_integration_feal_decrypt_option_output_format_hex(self):
+    def test_integration_feal_decrypt_output_format_hex(self):
         p = feal()
         self.assertEqual(p, '0x0')
