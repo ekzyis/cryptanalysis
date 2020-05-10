92faefafa10e20662ac19d6b2ce3c697f9e87394
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 12:05:17 +0200

    Fix tests for min_bits

--- test/util/test_min_bits.py
+++ test/util/test_min_bits.py
@@ -9,8 +9,8 @@ class TestMinBits(unittest.TestCase):
     def test_min_bits(self):
         self.assertEqual(min_bits(8), 4)
         self.assertEqual(min_bits(7), 3)
-        self.assertEqual(min_bits(256), 8)
-        self.assertEqual(min_bits(255), 7)
-        self.assertEqual(min_bits(129), 7)
-        self.assertEqual(min_bits(128), 7)
-        self.assertEqual(min_bits(127), 6)
+        self.assertEqual(min_bits(256), 9)
+        self.assertEqual(min_bits(255), 8)
+        self.assertEqual(min_bits(129), 8)
+        self.assertEqual(min_bits(128), 8)
+        self.assertEqual(min_bits(127), 7)
