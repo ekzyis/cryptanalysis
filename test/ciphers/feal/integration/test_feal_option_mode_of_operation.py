be980b5a8e3b18b1eb86e5355840a033fdfa0866
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 14:01:51 +0200

    Use lower hexadecimal strings

--- test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
+++ test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
@@ -9,18 +9,18 @@ from test.ciphers.feal.integration.wrappers import default_decrypt_args, default
 class TestFeal(unittest.TestCase):
 
     def test_integration_feal_option_mode_of_operation_ecb_with_no_remainder(self):
-        @default_encrypt_args('-m', 'ecb', key='0xFFFF', text='0xFFFAFFFAFFFAFFFAFFFAFFFAFFFAFFFA')
+        @default_encrypt_args('-m', 'ecb', key='0xffff', text='0xfffafffafffafffafffafffafffafffa')
         def test_mode_of_operation_ecb_with_encrypt():
             c = feal()
             #              KEY    TEXT                 OUTPUT IN HEX
-            # feal encrypt 0xFFFF 0xFFFAFFFAFFFAFFFA   0xa281baaa4303e813
-            self.assertEqual(hex(int(c, 0)), '0xa281baaa4303e813a281baaa4303e813')
+            # feal encrypt 0xffff 0xfffafffafffafffa   0xa281baaa4303e813
+            self.assertEqual(hex(int(c, 0)).lower(), '0xa281baaa4303e813a281baaa4303e813')
 
         test_mode_of_operation_ecb_with_encrypt()
 
-        @default_decrypt_args('-m', 'ecb', key='0xFFFF', text='0xa281baaa4303e813a281baaa4303e813')
+        @default_decrypt_args('-m', 'ecb', key='0xffff', text='0xa281baaa4303e813a281baaa4303e813')
         def test_mode_of_operation_ecb_with_decrypt():
             p = feal()
-            self.assertEqual(hex(int(p, 0)), '0xFFFAFFFAFFFAFFFAFFFAFFFAFFFAFFFA')
+            self.assertEqual(hex(int(p, 0)).lower(), '0xfffafffafffafffafffafffafffafffa')
 
         test_mode_of_operation_ecb_with_decrypt()
