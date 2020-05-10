9f6510f3b07ef9e15b24c7534e2ff37ab4fddd55
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 19:17:43 +0200

    Just compare numbers instead of strings

--- test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
+++ test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
@@ -13,12 +13,12 @@ class TestFEALOptionModeOfOperation(unittest.TestCase):
         c = feal()
         #              KEY    TEXT                  OUTPUT IN HEX
         # feal encrypt 0xffff 0xfffafffafffafffa    0xa281baaa4303e813
-        self.assertEqual(hex(int(c, 0)).lower(), '0xa281baaa4303e813a281baaa4303e813')
+        self.assertEqual(int(c), 0xa281baaa4303e813a281baaa4303e813)
 
     @default_decrypt_args('-m', 'ecb', key='0xffff', text='0xa281baaa4303e813a281baaa4303e813')
     def test_integration_feal_decrypt_mode_of_operation_ecb_no_remainder(self):
         p = feal()
-        self.assertEqual(hex(int(p, 0)).lower(), '0xfffafffafffafffafffafffafffafffa')
+        self.assertEqual(int(p), 0xfffafffafffafffafffafffafffafffa)
 
     @default_encrypt_args('-m', 'ecb', key='0xffff', text='0xfffafffafffafffafffafffa')
     def test_integration_feal_encrypt_mode_of_operation_ecb_with_remainder(self):
@@ -26,12 +26,12 @@ class TestFEALOptionModeOfOperation(unittest.TestCase):
         #              KEY    TEXT                  OUTPUT IN HEX
         # feal encrypt 0xffff 0xfffafffa            0x1ab00c4bbbc208e6
         # feal encrypt 0xffff 0xfffafffafffafffa    0xa281baaa4303e813
-        self.assertEqual(hex(int(c, 0)).lower(), '0x1ab00c4bbbc208e6a281baaa4303e813')
+        self.assertEqual(int(c), 0x1ab00c4bbbc208e6a281baaa4303e813)
 
     @default_decrypt_args('-m', 'ecb', key='0xffff', text='0x1ab00c4bbbc208e6a281baaa4303e813')
     def test_integration_feal_decrypt_mode_of_operation_ecb_with_remainder(self):
         p = feal()
-        self.assertEqual(hex(int(p, 0)).lower(), '0xfffafffafffafffafffafffa')
+        self.assertEqual(int(p), 0xfffafffafffafffafffafffa)
 
     @default_encrypt_args('-m', 'invalid')
     def test_integration_feal_encrypt_mode_of_operation_raises_error_on_invalid_mode_input(self):
