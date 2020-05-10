1f2ecdc9b38a9239891a0ff59d44fd0c767730dd
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 12:58:15 +0200

    Mock now in setUp

--- test/ciphers/modi/test_ecb.py
+++ test/ciphers/modi/test_ecb.py
@@ -10,12 +10,12 @@ from ciphers.modi.ecb import ecb
 class TestECB(unittest.TestCase):
 
     def setUp(self):
-        self.cipher_fn = lambda k, m: m >> 4
+        self.cipher_fn = mock.Mock(wraps=lambda k, m: m >> 4)
 
     def test_ecb_blocksize_16_with_no_remainder_in_blocks(self):
         m = 0x1234ABCD5678
         k = None
-        cipher_fn = mock.Mock(wraps=self.cipher_fn)
+        cipher_fn = self.cipher_fn
         c = ecb(cipher_fn, blocksize=16)(k, m)
         self.assertEqual(cipher_fn.call_count, 3)
         cipher_fn.assert_has_calls([call(k, 0x1234), call(k, 0xABCD), call(k, 0x5678)])
@@ -24,7 +24,7 @@ class TestECB(unittest.TestCase):
     def test_ecb_blocksize_16_with_message_which_has_leading_zeroes(self):
         m = 0x00123456
         k = None
-        cipher_fn = mock.Mock(wraps=self.cipher_fn)
+        cipher_fn = self.cipher_fn
         c = ecb(cipher_fn, blocksize=16)(k, m)
         self.assertEqual(cipher_fn.call_count, 2)
         cipher_fn.assert_has_calls([call(k, 0x0012), call(k, 0x3456)])
