5c2b8a0b542ed2926bc4ce440f883ba8eab1143a
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-10 12:40:29 +0200

    Fix casting from integer string to hex

--- test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
+++ test/ciphers/feal/integration/test_feal_option_mode_of_operation.py
@@ -15,13 +15,13 @@ class TestFeal(unittest.TestCase):
             #              KEY    TEXT                  OUTPUT IN HEX
             # feal encrypt 0xFFFF 0xFFFFAAAAFFFFAAAA    0xf4090052cbdac300
             # feal encrypt 0xFFFF 0xCCCCDDDD            0xd8428cef9fefca96
-            self.assertEqual(hex(c), '0xf4090052cbdac300d8428cef9fefca96')
+            self.assertEqual(hex(int(c, 16)), '0xf4090052cbdac300d8428cef9fefca96')
 
         test_mode_of_operation_ecb_with_encrypt()
 
         @default_decrypt_args('-m', 'ecb', key='0xFFFF', text='0xf4090052cbdac300d8428cef9fefca96')
         def test_mode_of_operation_ecb_with_decrypt():
             p = feal()
-            self.assertEqual(hex(p), '0xFFFFAAAAFFFFAAAACCCCDDDD')
+            self.assertEqual(hex(int(p, 16)), '0xFFFFAAAAFFFFAAAACCCCDDDD')
 
         test_mode_of_operation_ecb_with_decrypt()
