1120236778ad4a69dc7712e88f9fc463c3df0d4b
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-12 13:50:10 +0200

    Added integration test for ecb and encode wrappers

---
+++ test/ciphers/feal/integration/test_options_wrapping.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest import mock
+from unittest.mock import call
+
+from ciphers.modi.ecb import ecb
+from util.encode import encode, encode_wrapper
+
+
+class TestOptionsWrapping(unittest.TestCase):
+    def test_ecb_with_encode_wrapper(self):
+        encrypt = mock.Mock()
+        ecb_wrap = mock.Mock(wraps=ecb(encrypt, 8))
+        k, m = 0xffff, "test"
+        encode_wrapper(ecb_wrap)(k, m)
+        ecb_wrap.assert_called_once_with(k, encode(m))
+        encrypt.assert_has_calls([call(k, b) for b in ecb_wrap(k, encode(m))])
