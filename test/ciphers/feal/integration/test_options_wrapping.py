9a644c1ef1eae7ed3733efd59143ba2fa3a75c64
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-12 14:04:10 +0200

    test_ecb_with_encode_wrapper: PASS

--- test/ciphers/feal/integration/test_options_wrapping.py
+++ test/ciphers/feal/integration/test_options_wrapping.py
@@ -8,9 +8,14 @@ from util.encode import encode, encode_wrapper
 
 class TestOptionsWrapping(unittest.TestCase):
     def test_ecb_with_encode_wrapper(self):
-        encrypt = mock.Mock(wraps=lambda _k, _m: _m)
+        encrypt = mock.Mock(wraps=lambda _k, _m: _m >> 4)
         ecb_wrap = mock.Mock(wraps=ecb(encrypt, 8))
         k, m = 0xffff, "test"
-        encode_wrapper(ecb_wrap)(k, m)
+        o = encode_wrapper(ecb_wrap)(k, m)
         ecb_wrap.assert_called_once_with(k, encode(m))
-        encrypt.assert_has_calls([call(k, b) for b in ecb_wrap(k, encode(m))])
+        # encode(test) == 0x74657374
+        #   ecb should split this up into four 8-bit strings:
+        #   0x74, 0x65, 0x73, 0x74
+        self.assertEqual(encrypt.call_count, 4)
+        encrypt.assert_has_calls([call(k, 0x74), call(k, 0x65), call(k, 0x73), call(k, 0x74)])
+        self.assertEqual(o, 0x07060707)
