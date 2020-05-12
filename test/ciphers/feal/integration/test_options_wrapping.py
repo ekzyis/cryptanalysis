97028b19a9952124185873481298529ac0199039
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-12 14:11:54 +0200

    Remove shift because it is not reversible
    thus makes problem when decrypting

--- test/ciphers/feal/integration/test_options_wrapping.py
+++ test/ciphers/feal/integration/test_options_wrapping.py
@@ -8,7 +8,7 @@ from util.encode import encode, encode_wrapper
 
 class TestOptionsWrapping(unittest.TestCase):
     def test_ecb_with_encode_wrapper(self):
-        encrypt = mock.Mock(wraps=lambda _k, _m: _m >> 4)
+        encrypt = mock.Mock(wraps=lambda _k, _m: _m)
         ecb_wrap = mock.Mock(wraps=ecb(encrypt, 8))
         k, m = 0xffff, "test"
         o = encode_wrapper(ecb_wrap)(k, m)
@@ -18,4 +18,4 @@ class TestOptionsWrapping(unittest.TestCase):
         #   0x74, 0x65, 0x73, 0x74
         self.assertEqual(encrypt.call_count, 4)
         encrypt.assert_has_calls([call(k, 0x74), call(k, 0x65), call(k, 0x73), call(k, 0x74)])
-        self.assertEqual(o, 0x07060707)
+        self.assertEqual(o, 0x74657374)
