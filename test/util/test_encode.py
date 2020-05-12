9bc792434af5c339b8758bb54a21965883d6db1c
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-12 13:18:38 +0200

    Added encode/decode wrapper tests

--- test/util/test_encode.py
+++ test/util/test_encode.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest import mock
 
-from util.encode import encode, decode
+from util.encode import encode, decode, encode_wrapper, decode_wrapper
 
 
 class TestEncode(unittest.TestCase):
@@ -19,3 +20,16 @@ class TestEncode(unittest.TestCase):
     def test_encode_decode_round_trip_with_special_characters(self):
         test = '!"§$%&/()=?`²³{[]}\\´öäü+*~#\'<>|^°,;.:-_@ł€¶ŧ←↓→øþ¨æſðđŋħ̣̣ĸł˝»«¢„“”µ·…–'
         self.assertEqual(decode(encode(test)), test)
+
+    def test_encode_wrapper(self):
+        cipher_fn = mock.Mock()
+        k, m = 0xffff, "test"
+        encode_wrapper(cipher_fn)(k, m)
+        cipher_fn.assert_called_once_with(k, encode(m))
+
+    def test_decode_wrapper(self):
+        cipher_fn = mock.Mock(wraps=lambda k, m: m)
+        k, m = 0xffff, 0x1234
+        o = decode_wrapper(cipher_fn)(k, m)
+        cipher_fn.assert_called_once_with(k, m)
+        self.assertEqual(o, decode(m))
