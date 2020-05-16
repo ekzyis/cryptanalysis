1bfe3421c15879d7524a096e5f14bc946c810910
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-16 12:29:02 +0200

    Resolved pydocstyle issues in concat_bits.py

--- src/util/concat_bits.py
+++ src/util/concat_bits.py
@@ -1,7 +1,13 @@
+"""Exports function which concatenates bitstrings into one bitstring."""
+
+
 def concat_bits(*args, n):
-    """Concatenates the given subkeys into one key.
-    The first key is shifted to the highest n-bits, the second key to the second highest n-bits and so forth.
-    Raises error when one subkey is larger than n."""
+    """Concatenates the given bitstring arguments into one bitstring.
+
+    The first key is shifted to the most significant n-bits, the second key to the second most significant n-bits
+    and so forth.
+    Raises error when one subkey is larger than n.
+    """
     args = list(args)  # convert args to list because *args creates tuple
     args.reverse()
     r = 0
