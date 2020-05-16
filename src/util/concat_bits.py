2139532cd212235196395f3a5cb68cd74536d454
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-16 17:32:01 +0200

    Add type hinting to concat_bits

--- src/util/concat_bits.py
+++ src/util/concat_bits.py
@@ -1,17 +1,17 @@
 """Exports function which concatenates bitstrings into one bitstring."""
 
 
-def concat_bits(*args, n):
+def concat_bits(*args: int, n: int) -> int:
     """Concatenates the given bitstring arguments into one bitstring.
 
     The first key is shifted to the most significant n-bits, the second key to the second most significant n-bits
     and so forth.
     Raises error when one subkey is larger than n.
     """
-    args = list(args)  # convert args to list because *args creates tuple
-    args.reverse()
+    args_ = list(args)  # convert args to list because *args creates tuple
+    args_.reverse()
     r = 0
-    for i, arg in enumerate(args):
+    for i, arg in enumerate(args_):
         if arg > 2 ** n:
             raise ValueError("subkey is larger than slot given by n")
         r |= arg << (i * n)
