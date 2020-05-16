624cad6ad442d77395480a71c413d8fefe224eca
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-16 14:08:02 +0200

    Added __doc__ 's to patchers.py

--- test/ciphers/feal/integration/patchers.py
+++ test/ciphers/feal/integration/patchers.py
@@ -1,7 +1,15 @@
+"""Decorators used for integration tests.
+
+Exports decorators for encrypt and decrypt which simulates the behaviour of passing arguments via command line
+by mocking `sys.argv`.
+"""
+
 from unittest import mock
 
 
 def sysv_patcher(*default_args, **default_kwargs):
+    """Overwrite `sys.argv` by using `mock.patch` with given arguments."""
+
     def additional_args_wrapper(*add_args, **add_kwargs):
         kwargs = dict(default_kwargs, **add_kwargs)
         args = list(default_args + add_args) + [kwargs['key'], kwargs['text']]
