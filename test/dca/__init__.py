999e90e08916c54faefdc4bd97b7ca044998e7c2
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-16 14:01:07 +0200

    Added module __doc__

---
+++ test/dca/__init__.py
@@ -0,0 +1,5 @@
+"""Tests to assert statements of C. Bender's master thesis about Differential Cryptanalysis.
+
+Thesis can be found at:
+    https://www.cryptool.org/images/ctp/documents/MA_Bender.pdf
+"""
