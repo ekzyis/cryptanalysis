56af37efcb90028dbc803ff77bae385a575bfaaa
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-16 17:50:56 +0200

    Added link related github issue

--- src/util/types.py
+++ src/util/types.py
@@ -7,4 +7,5 @@ class CipherFunction(Protocol):
     # NOTE text should actually be hinted with Union[int,str]
     # NOTE return value should actually be hinted with Union[int,str]
     # But when hinting with Union[int, str] `mypy` also throws error when hinting a function with only int or str.
+    # https://github.com/python/mypy/issues/7183
     def __call__(self, key: int, text: Any, *args: Any, **kwargs: Any) -> Any: ...
