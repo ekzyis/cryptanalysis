6744c864e6fc995c52d12b6d42653246a4e009d7
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-18 13:29:13 +0200

    Update type hinting in Word due to tuple syntax

--- src/util/word.py
+++ src/util/word.py
@@ -1,14 +1,17 @@
 """Utility class to store bitstrings in a more readable way."""
 
 from itertools import chain
+from typing import List, Any
 
 from util.concat_bits import concat_bits
 
 
 class Word(int):
-    def __new__(cls, *args: int, bit: int = 32) -> 'Word':
+    # TODO args should actually be hinted with Union[int, Tuple[int, ...]]
+    def __new__(cls, *args: Any, bit: int = 32) -> 'Word':
         """Creates an actual integer out of the word representation given with args and bit."""
+        _args: List[Any] = list(args)
         if isinstance(args[0], tuple):
-            args = [a for a in chain(*args)]
+            _args = [a for a in chain(*args)]
         # TODO mypy throws error: Too many arguments for "__new__" of "object"
-        return int.__new__(cls, concat_bits(*args, n=bit))
+        return int.__new__(cls, concat_bits(*_args, n=bit))
