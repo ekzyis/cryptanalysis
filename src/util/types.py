b07a11744a3523142c3a5ad60a987a1a41290fdc
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-18 11:03:31 +0200

    Added missing __doc__ to types.CipherFunction

--- src/util/types.py
+++ src/util/types.py
@@ -4,11 +4,15 @@ from typing import Protocol, Any, TypedDict, Callable
 
 
 class CipherFunction(Protocol):
+    """Class for type hinting cipher functions (functions with signature as __call__)."""
+
     # NOTE text should actually be hinted with Union[int,str]
     # NOTE return value should actually be hinted with Union[int,str]
     # But when hinting with Union[int, str] `mypy` also throws error when hinting a function with only int or str.
     # https://github.com/python/mypy/issues/7183
-    def __call__(self, key: int, text: Any, *args: Any, **kwargs: Any) -> Any: ...
+    def __call__(self, key: int, text: Any, *args: Any, **kwargs: Any) -> Any:
+        """Cipher functions should have as first argument a key as int, and second the text to en- or decrypt."""
+        pass
 
 
 Formatter = Callable[[Any], Any]
