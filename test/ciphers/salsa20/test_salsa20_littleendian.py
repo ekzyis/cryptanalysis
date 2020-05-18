6432a0ce002b0833c8fb145742e40b25823362f4
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-18 15:26:07 +0200

    Fix test data

--- test/ciphers/salsa20/test_salsa20_littleendian.py
+++ test/ciphers/salsa20/test_salsa20_littleendian.py
@@ -10,4 +10,4 @@ class TestSalsa20CipherLittleEndian(unittest.TestCase):
     def test_salsa20_littleendian(self):
         self.assertEqual(littleendian(0x0), 0x0)
         self.assertEqual(littleendian(Word(0x56, 0x4b, 0x1e, 0x9, bit=8)), 0x091e4b56)
-        self.assertEqual(littleendian(Word(0xff, 0xff, 0xff, 0xfa)), 0xfaffffff)
+        self.assertEqual(littleendian(Word(0xff, 0xff, 0xff, 0xfa, bit=8)), 0xfaffffff)
