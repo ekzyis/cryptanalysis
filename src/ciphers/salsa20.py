489dfc56e3b4c876b0d40c52bd3f6b34c28a954e
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-18 12:28:12 +0200

    test_salsa20_quarterround: PASS

--- src/ciphers/salsa20.py
+++ src/ciphers/salsa20.py
@@ -16,8 +16,19 @@ Usage:
     PLAINTEXT               The text to encrypt. Must be a number. Can be a code literal such as 0b1011, 0o71, 0xF32C.
     CIPHERTEXT              The text to decrypt. Must be a number. Can be a code literal such as 0b1011, 0o71, 0xF32C.
 """
+from typing import List
+
+from util.rot import rot_left
+from util.split import split
+from util.word import Word
 
 
 def quarterround(y: int) -> int:
     """The quarterround function of Salsa20."""
-    pass
+    y_: List[int] = split(4, 32, y)
+    y0, y1, y2, y3 = y_
+    z1 = y1 ^ rot_left((y0 + y3) % (2 ** 32), 7, 32)
+    z2 = y2 ^ rot_left((z1 + y0) % (2 ** 32), 9, 32)
+    z3 = y3 ^ rot_left((z2 + z1) % (2 ** 32), 13, 32)
+    z0 = y0 ^ rot_left((z3 + z2) % (2 ** 32), 18, 32)
+    return Word(z0, z1, z2, z3, bit=32)
