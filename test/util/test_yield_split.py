09cb2ef1d6068cffeea09eef8d75cbd1c139699c
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-21 22:03:48 +0200

    Added another test for yield_split

--- test/util/test_yield_split.py
+++ test/util/test_yield_split.py
@@ -11,3 +11,8 @@ class TestLimit(unittest.TestCase):
         m = 0xf1a3b7ae9281
         blocks = [b for b in yield_split(16, m)]
         self.assertEqual(blocks, [0xf1a3, 0xb7ae, 0x9281])
+
+    def test_yield_split_returns_blocks_of_given_size_except_last_block(self):
+        m = 0xf1a3b7ae9281
+        blocks = [b for b in yield_split(32, m)]
+        self.assertEqual(blocks, [0xf1a3b7ae, 0x9281])
