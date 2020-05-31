a899eb7df5a2cdf2b8a7945c46ad4d4c4e6cd941
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-31 17:49:49 +0200

    Added tests for ciphertext

--- test/ciphers/salsa20/test_salsa20_encrypt.py
+++ test/ciphers/salsa20/test_salsa20_encrypt.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 # noinspection PyUnresolvedReferences
 import test.context
-from ciphers.salsa20 import expansion
+from ciphers.salsa20 import expansion, encrypt
 from util.word import Word
 
 
@@ -66,6 +66,11 @@ class TestSalsa20CipherEncrypt(unittest.TestCase):
             (0x4EC4D595, 0xE85225F0, 0x8E2BC03F, 0xE1C42567),
             bit=32
         ))
+        m = 0x0
+        c = encrypt(k, m)
+        self.assertEqual(c, Word(
+            *[stream[i] for i in range(8)], bit=512
+        ))
 
     @mock.patch('random.randint', return_value=0x0)
     def test_salsa20_encrypt_256_bit_key_2(self, iv):
@@ -118,6 +123,11 @@ class TestSalsa20CipherEncrypt(unittest.TestCase):
             (0x3B820711, 0x202105D1, 0x20398ECB, 0x96C0C102),
             bit=32
         ))
+        m = 0x0
+        c = encrypt(k, m)
+        self.assertEqual(c, Word(
+            *[stream[i] for i in range(8)], bit=512
+        ))
 
     @mock.patch('random.randint', return_value=0x0)
     def test_salsa20_encrypt_256_bit_key_3(self, iv):
@@ -169,4 +179,9 @@ class TestSalsa20CipherEncrypt(unittest.TestCase):
             (0x872C5D96, 0xCDC25074, 0x6C26BD80, 0x3903E28D),
             (0x7DEC66DE, 0xD9AB7DE6, 0x797C502B, 0x3D1B246D),
             bit=32
+        ))
+        m = 0x0
+        c = encrypt(k, m)
+        self.assertEqual(c, Word(
+            *[stream[i] for i in range(8)], bit=512
         ))
