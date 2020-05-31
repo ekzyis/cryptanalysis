4925726df36a74ce4dc2e727d71eafb032ed39bb
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-31 19:40:59 +0200

    test_salsa20_encrypt_256_bit_key_1_with_non_zero_512_bit_plaintext: PASS

--- test/ciphers/salsa20/test_salsa20_encrypt.py
+++ test/ciphers/salsa20/test_salsa20_encrypt.py
@@ -74,6 +74,58 @@ class TestSalsa20CipherEncrypt(unittest.TestCase):
         ))
 
     @mock.patch('random.randint', return_value=0x0)
+    def test_salsa20_encrypt_256_bit_key_1_with_non_zero_512_bit_plaintext(self, iv):
+        k = Word((0x80000000, 0x0, 0x0, 0x0),
+                 (0x00000000, 0x0, 0x0, 0x0),
+                 bit=32)
+        # 64 bit * 4 * 16 = 256 bit * 16 = 512 bit * 8 = 512 bytes
+        m = Text(
+            0xdf31a36fbdd19f0d, 0x57519a1a8a9e677b, 0xa7037dc1e72595cb, 0x86ae61b858605ccc,
+            0xc3045206b2437afc, 0xe21e6a5c66f631fe, 0xb43a64b51ea2c489, 0x860d212a7c9a72fd,
+            0x8ada021ed41e0c95, 0x1f9c8cc391aa8bbc, 0xf4b9d53d3ab1bc13, 0xd950c1ea2762614e,
+            0xb115e14e4fdb0c9d, 0x4a678418995c1a9a, 0x61ccda166f77c262, 0x667430d278b5c107,
+
+            0x722b2c668370a829, 0x6bcc754023701aa1, 0xd6358099024336b5, 0xa5db1cd4aac92019,
+            0x406278ada2dab814, 0x3bdee36b7885605d, 0x4f4abe76d7a8d507, 0x639c8dbbc15a0b03,
+            0xf8c38747894e9e38, 0x55caef1fd0fd04b7, 0xbeff18c08c633936, 0x69c9cc6d6d2741fb,
+            0x4fcdd255bdd31fa1, 0x1a4ece13df801d2f, 0x5835f331063f2477, 0x2ad5ae463c36ad36,
+
+            0x5de629ce415a1a06, 0x278dc0e833608a84, 0x6e4073481eb82b8c, 0x86095a3313901208,
+            0xb718a3531541fac4, 0xcc13466859ba677c, 0xd026a1897f6cc9ac, 0x473bf708e696bff4,
+            0xe961e55e272a88cd, 0x25626d43952c4d8f, 0xbaae1ad014f86193, 0xd2096718f6883a2e,
+            0xb7b188fd3034eb6c, 0x11c45600ea3fc1fc, 0x6f98b86f0f451264, 0x0309ac1c004e4f90,
+
+            0xcad8c12db9a129a2, 0x06c622c3cdf6022b, 0x918830f09eb7195d, 0x2ef0acfc26b8a5a1,
+            0x8dedc629d734375d, 0xabe1f85b09f7af52, 0xaee3304baa413a3b, 0xa1d87f9a6c96ab7c,
+            0x3e3c9d6a9a839202, 0xeed50cadff279074, 0x2fcb41ea235aedcc, 0xc73f0ce821ba342e,
+            0xd8ba37249c59f87d, 0x2b3ddb44d2e392a5, 0xacb4a32cf7272f4e, 0xd1153624755c6a38,
+            bit=64)
+        c = encrypt(k, m)
+        stream = Word(
+            0xe3be8fdd8beca2e3, 0xea8ef9475b29a6e7, 0x003951e1097a5c38, 0xd23b7a5fad9f6844,
+            0xb22c97559e2723c7, 0xcbbd3fe4fc8d9a07, 0x44652a83e72a9c46, 0x1876af4d7ef1a117,
+            0x8da2b74eef1b6283, 0xe7e20166abcae538, 0xe9716e4669e2816b, 0x6b20c5c356802001,
+            0xcc1403a9a117d12a, 0x2669f456366d6ebb, 0x0f1246f1265150f7, 0x93cdb4b253e348ae,
+
+            0x203d89bc025e802a, 0x7e0e00621d70aa36, 0xb7e07cb1e7d5b38d, 0x5e222b8b0e4b8407,
+            0x0142b1e29504767d, 0x76824850320b5368, 0x129fdd74e861b498, 0xe3be8d16f2d7d169,
+            0x57be81f47b17d9ae, 0x7c4ff15429a73e10, 0xacf250ed3a90a93c, 0x711308a74c6216a9,
+            0xed84cd126da7f28e, 0x8abf8bb63517e1ca, 0x98e712f4fb2e1a6a, 0xed9fdc73291faa17,
+
+            0x958211c4ba2ebd58, 0x38c635edb81f513a, 0x91a294e194f1c039, 0xaeec657dce40aa7e,
+            0x7c0af57cacefa40c, 0x9f14b71a4b3456a6, 0x3e162ec7d8d10b8f, 0xfb1810d71001b618,
+            0x2f9f73da53b85405, 0xc11f7b2d890fa8ae, 0x0c7f2e926d8a98c7, 0xec4e91b65120e988,
+            0x349631a700c6face, 0xc3471cb0413656e7, 0x5e309456584084d7, 0xe12c5b43a41c43ed,
+
+            0x9a048abd9b880da6, 0x5f6a665a20fe7b77, 0xcd292fe62cae644b, 0x7f7df69f32bdb331,
+            0x903e6505ce44fdc2, 0x93920c6a9ec7057e, 0x23df7dad298f82dd, 0xf4efb7fdc7bfc622,
+            0x696afcfd0cddcc83, 0xc7e77f11a649d79a, 0xcdc3354e9635ff13, 0x7e929933a0bd6f53,
+            0x77efa105a3a4266b, 0x7c0d089d08f1e855, 0xcc32b15b93784a36, 0xe56a76cc64bc8477,
+            bit=64
+        )
+        self.assertEqual(c, m ^ stream)
+
+    @mock.patch('random.randint', return_value=0x0)
     def test_salsa20_encrypt_256_bit_key_2(self, iv):
         """Key size: 256 bits, IV size: 64 bits, Test vectors -- set 1, vector# 9."""
         k = Word((0x00400000, 0x0, 0x0, 0x0),
