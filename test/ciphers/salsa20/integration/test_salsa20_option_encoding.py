155fd9ed35dbc0d53de84e0d94f1263b0ec7a0c7
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-05 22:54:49 +0200

    Added tests for encoding option
    test_integration_salsa20_encrypt_encoding_utf8: PASS
    test_integration_salsa20_decrypt_encoding_utf8: FAIL
    test_integration_salsa20_encrypt: FAIL
    test_integration_salsa20_decrypt: FAIL

---
+++ test/ciphers/salsa20/integration/test_salsa20_option_encoding.py
@@ -0,0 +1,22 @@
+import unittest
+
+# noinspection PyUnresolvedReferences
+import test.context
+from ciphers.salsa20 import salsa20
+from test.ciphers.salsa20.integration.patchers import default_encrypt_args, default_decrypt_args
+
+
+class TestSalsa20OptionEncoding(unittest.TestCase):
+
+    @default_encrypt_args('-x', 'utf8', key="0xffff", text="secret")
+    def test_integration_salsa20_encrypt_encoding_utf8(self, _):
+        c = salsa20()
+        # "secret".encode('utf8').hex()         => 0x736563726574
+        # IV: 0x0
+        # salsa20 encrypt 0xffff 0x736563726574 => 0x187383703ca5
+        self.assertEqual(int(c), 0x187383703ca5)
+
+    @default_decrypt_args('-x', 'utf8', key="0xffff", text="0xc6c0acf427106a8d")
+    def test_integration_salsa20_decrypt_encoding_utf8(self):
+        p = salsa20()
+        self.assertEqual(p, "secret")
