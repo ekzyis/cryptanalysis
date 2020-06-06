603e7b67b044e2b5483ece386a662fb9ee176fbf
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-06 20:21:33 +0200

    Added custom bit assertion function

--- test/util/test_bitseq.py
+++ test/util/test_bitseq.py
@@ -9,27 +9,31 @@ from util.bitseq import bitseq, bitseq_from_str
 
 class TestBitSeq(unittest.TestCase):
 
+    def setUp(self):
+        def assert_bit(b: bitstring.Bits, value: int, length: int):
+            """Custom bit assertion function."""
+            self.assertEqual(b.uint, value)
+            self.assertEqual(len(b), length)
+
+        self.assertBit = assert_bit
+
     def test_bitseq_returns_bitstring_Bits_instance(self):
         b = bitseq(0x1, 0x2, 0x3, 0x4, bit=4)
         self.assertIsInstance(b, bitstring.Bits)
 
     def test_bitseq_with_only_integer_arguments(self):
         b1 = bitseq(0x1, 0x2, 0x3, 0x4, bit=4)
-        self.assertEqual(b1.uint, 0x1234)
-        self.assertEqual(len(b1), 16)
+        self.assertBit(b1, 0x1234, 16)
         b2 = bitseq(0x1, 0x2, 0x3, 0x4, bit=16)
-        self.assertEqual(b2.uint, 0x0001000200030004)
-        self.assertEqual(len(b2), 64)
+        self.assertBit(b2, 0x0001000200030004, 64)
 
     def test_bitseq_with_zero_bits(self):
         # leading zeros
         b1 = bitseq(0x0, 0x0, 0xa, 0xf, bit=4)
-        self.assertEqual(b1.uint, 0xaf)
-        self.assertEqual(len(b1), 16)
+        self.assertBit(b1, 0xaf, 16)
         # only zeros
         b2 = bitseq(0x0, 0x0, 0x0, 0x0, bit=4)
-        self.assertEqual(b2.uint, 0x0)
-        self.assertEqual(len(b2), 16)
+        self.assertBit(b2, 0x0, 16)
 
     def test_bitseq_raises_value_error_if_integer_too_large(self):
         with self.assertRaises(ValueError):
@@ -41,8 +45,6 @@ class TestBitSeq(unittest.TestCase):
 
     def test_bitseq_from_str(self):
         b1 = bitseq_from_str("0x1", "0x2", "0x3", "0x4")
-        self.assertEqual(b1.uint, 0x1234)
-        self.assertEqual(len(b1), 16)
+        self.assertBit(b1, 0x1234, 16)
         b2 = bitseq_from_str("0x01", "0x02", "0x03", "0x04")
-        self.assertEqual(b2.uint, 0x01020304)
-        self.assertEqual(len(b2), 32)
+        self.assertBit(b2, 0x01020304, 32)
