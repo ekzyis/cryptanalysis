ab03fa0fa8c38decb8aa60dfe7b32bbdb3f0c522
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-07 18:57:37 +0200

    Use default key for encrypt decrypt encoding integration test

--- test/ciphers/salsa20/integration/test_salsa20_option_encoding.py
+++ test/ciphers/salsa20/integration/test_salsa20_option_encoding.py
@@ -8,15 +8,15 @@ from test.ciphers.salsa20.integration.patchers import default_encrypt_args, defa
 
 class TestSalsa20OptionEncoding(unittest.TestCase):
 
-    @default_encrypt_args('-x', 'utf8', key="0xffff", text="secret")
+    @default_encrypt_args('-x', 'utf8', text="secret")
     def test_integration_salsa20_encrypt_encoding_utf8(self, _):
-        c = salsa20()
         # "secret".encode('utf8').hex()         => 0x736563726574
-        # IV: 0x0
-        # salsa20 encrypt 0xffff 0x736563726574 => 0xa548a376d6d7
-        self.assertEqual(int(c), 0xa548a376d6d7)
+        # IV:  0x0
+        # key: default
+        # salsa20 encrypt key 0x736563726574 => 0x000000000000000090dbecafee98
+        self.assertEqual(salsa20(), "0x000000000000000090dbecafee98")
 
-    @default_decrypt_args('-x', 'utf8', key="0xffff", text="0xc6c0acf427106a8d")
+    @default_decrypt_args('-x', 'utf8', text="0x000000000000000090dbecafee98")
     def test_integration_salsa20_decrypt_encoding_utf8(self):
         p = salsa20()
         self.assertEqual(p, "secret")
