7bdee7650e08b532e6be333398b5099c38f67ca9
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-08 22:55:35 +0200

    Fix default plaintext not 64-bit

--- test/ciphers/feal/integration/patchers.py
+++ test/ciphers/feal/integration/patchers.py
@@ -2,7 +2,8 @@
 
 from test.ciphers.sysv_patcher import sysv_patcher
 
-default_encrypt_args = sysv_patcher('feal', 'encrypt', key='0x0123456789ABCDEF0123456789ABCDEF', text='0x0')
+default_encrypt_args = sysv_patcher('feal', 'encrypt', key='0x0123456789ABCDEF0123456789ABCDEF',
+                                    text='0x0000000000000000')
 
 default_decrypt_args = sysv_patcher('feal', 'decrypt', key='0x0123456789ABCDEF0123456789ABCDEF',
                                     text='0x9C9B54973DF685F8')
