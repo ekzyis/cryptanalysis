1e868977cd64752f4c47b004ec8c2d6510244f77
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-08 21:57:12 +0200

    Make test_integration_feal_decrypt runnable
    First iteration on FEAL rework with bitstring.Bits

--- src/ciphers/block/feal.py
+++ src/ciphers/block/feal.py
@@ -39,35 +39,36 @@ import sys
 from pathlib import Path
 from typing import Sequence, Tuple, Any, Union, Dict, Mapping, Optional
 
+from bitstring import Bits
 from docopt import docopt  # type: ignore
 
 # make sure that following imports can be resolved when executing this script from cmdline
-from ciphers.modi.ecb import ecb
-from util.encode import decode_wrapper, encode_wrapper
-
 sys.path.insert(0, str(Path(__file__).parent / '../..'))
 
+from ciphers.modi.ecb import ecb
+from util.bitseq import bitseq32, bitseq8, bitseq64
+from util.encode import decode_wrapper, encode_wrapper
 from ciphers.modi.wrap import key_input_to_bitseq_wrapper, text_input_to_bitseq_wrapper, output_wrapper
 from util.concat_bits import concat_bits
-from util.rot import rot_left
+from util.rot import rot_left_bits
 from util.split import split
 from util.types import CipherFunction, Formatter
 
 
-def key_schedule(key: int, n: int = 32) -> Sequence[int]:
+def key_schedule(key: Bits, n: int = 32) -> Sequence[Bits]:
     """Return the subkeys created by the key scheduler of FEAL-NX.
 
     Creates the N+8 16-bit subkeys which are needed during en-/decryption.
     Raises error if key is not 128-bit.
     """
-    if key >= 2 ** 128:
+    if len(key) != 128:
         raise ValueError("Key for FEAL-NX key scheduler must be 128-bit")
-    kl, kr = split(2, 64, key)
+    kl, kr = key[:64], key[64:]
     # processing of right key kr
-    kr1, kr2 = split(2, 32, kr)
+    kr1, kr2 = kr[:32], kr[32:]
     # insert "filler" element such that the the first added element is at index 1
     #   since in the specification, indices for q start with 1
-    q = [0x0]
+    q = [bitseq32(0x0)]
     for r in range(1, (int(n / 2) + 5)):
         if r % 3 == 1:
             q.append(kr1 ^ kr2)
@@ -76,37 +77,38 @@ def key_schedule(key: int, n: int = 32) -> Sequence[int]:
         elif r % 3 == 0:
             q.append(kr2)
     # processing of left key kl
-    a0, b0 = split(2, 32, kl)
+    a0, b0 = kl[:32], kl[32:]
     a = [a0]
     b = [b0]
-    d = [0x0]
+    d = [bitseq32(0x0)]
     k = []
     for r in range(1, int(n / 2) + 5):
         d.append(a[r - 1])
         a.append(b[r - 1])
         b.append(fk(a[r - 1], b[r - 1] ^ d[r - 1] ^ q[r]))
-        br0, br1, br2, br3 = split(4, 8, b[r])
-        k.append(concat_bits(br0, br1, n=8))
-        k.append(concat_bits(br2, br3, n=8))
+        br = b[r]
+        br0, br1, br2, br3 = br[:8], br[8:16], br[16:24], br[24:32]
+        k.append(br0 + br1)
+        k.append(br2 + br3)
     return k
 
 
-def s0(a: int, b: int) -> int:
+def s0(a: Bits, b: Bits) -> Bits:
     """Return substitution value of S-Box 0."""
     return _s(a, b, 0)
 
 
-def s1(a: int, b: int) -> int:
+def s1(a: Bits, b: Bits) -> Bits:
     """Return substitution value of S-Box 1."""
     return _s(a, b, 1)
 
 
-def _s(a: int, b: int, i: int) -> int:
+def _s(a: Bits, b: Bits, i: int) -> Bits:
     """General substitution box implementation for FEAL-NX."""
-    return rot_left((a + b + i) % 256, 2, 8)
+    return rot_left_bits(bitseq8((a.uint + b.uint + i) & 0xFF), 2)
 
 
-def f(a: int, b: int) -> int:
+def f(a: Bits, b: Bits) -> Bits:
     """f-function of FEAL-NX.
 
     a must be 32-bit and b must be 16-bit long.
@@ -114,12 +116,12 @@ def f(a: int, b: int) -> int:
     See section 5.1 and figure 3 in
     https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf
     """
-    if a >= 2 ** 32:
+    if len(a) != 32:
         raise ValueError("a key must be 32-bit")
-    if b >= 2 ** 16:
+    if len(b) != 16:
         raise ValueError("b key must be 16-bit")
-    a_k = split(n=4, size=8, bits=a)
-    b_k = split(n=2, size=8, bits=b)
+    a_k = a[0:4], a[4:8], a[8:12], a[12:16]
+    b_k = b[0:4], b[4:8], b[8:12], b[12:16]
     f1 = a_k[1] ^ b_k[0]
     f2 = a_k[2] ^ b_k[1]
     f1 ^= a_k[0]
@@ -128,10 +130,10 @@ def f(a: int, b: int) -> int:
     f2 = s0(f2, f1)
     f0 = s0(a_k[0], f1)
     f3 = s1(a_k[3], f2)
-    return concat_bits(f0, f1, f2, f3, n=8)
+    return sum([f0, f1, f2, f3])
 
 
-def fk(a: int, b: int) -> int:
+def fk(a: Bits, b: Bits) -> Bits:
     """f_k-function of FEAL-NX.
 
     Input keys must be 32-bit.
@@ -139,17 +141,17 @@ def fk(a: int, b: int) -> int:
     See section 5.2 and figure 4 in
     https://info.isl.ntt.co.jp/crypt/archive/dl/feal/call-3e.pdf
     """
-    if a >= 2 ** 32 or b >= 2 ** 32:
+    if len(a) != 32 or len(b) != 32:
         raise ValueError("Input keys must be 32-bit")
-    a_k = split(n=4, size=8, bits=a)
-    b_k = split(n=4, size=8, bits=b)
+    a_k = a[0:8], a[8:16], a[16:24], a[24:32]
+    b_k = b[0:8], b[8:16], b[16:24], b[24:32]
     fk1 = a_k[1] ^ a_k[0]
     fk2 = a_k[2] ^ a_k[3]
     fk1 = s1(fk1, fk2 ^ b_k[0])
     fk2 = s0(fk2, fk1 ^ b_k[1])
     fk0 = s0(a_k[0], fk1 ^ b_k[2])
     fk3 = s1(a_k[3], fk2 ^ b_k[3])
-    return concat_bits(fk0, fk1, fk2, fk3, n=8)
+    return sum([fk0, fk1, fk2, fk3])
 
 
 def _encrypt_preprocessing(subkeys: Sequence[int], text: int) -> int:
@@ -159,10 +161,10 @@ def _encrypt_preprocessing(subkeys: Sequence[int], text: int) -> int:
     return p
 
 
-def _decrypt_preprocessing(subkeys: Sequence[int], text: int) -> int:
-    p = text ^ concat_bits(*subkeys, n=16)
-    rn, ln = split(2, 32, p)
-    p = concat_bits(rn, ln, n=32) ^ rn
+def _decrypt_preprocessing(subkeys: Sequence[Bits], text: Bits) -> Bits:
+    p = text ^ sum(subkeys)
+    rn = p[:32]
+    p ^= bitseq64(rn)
     return p
 
 
@@ -175,8 +177,8 @@ def _encrypt_iterative_calculation(l0: int, r0: int, sk: Sequence[int], n: int =
     return l, r
 
 
-def _decrypt_iterative_calculation(ln: int, rn: int, sk: Sequence[int], n: int = 32) \
-        -> Tuple[Sequence[int], Sequence[int]]:
+def _decrypt_iterative_calculation(ln: Bits, rn: Bits, sk: Sequence[Bits], n: int = 32) \
+        -> Tuple[Sequence[Bits], Sequence[Bits]]:
     l, r = [0] * n + [ln], [0] * n + [rn]
     for i in reversed(range(1, n + 1)):
         l[i - 1] = r[i] ^ f(l[i], sk[i - 1])
@@ -208,27 +210,24 @@ def encrypt(key: int, text: int, *args: Any, **kwargs: Any) -> int:
     return c
 
 
-def decrypt(key: int, text: int, *args: Any, **kwargs: Any) -> int:
+def decrypt(key: Bits, text: Bits, *args: Any, **kwargs: Any) -> Bits:
     """Decrypt the ciphertext with the given key using FEAL-NX decryption.
 
     Raises error if text is longer than 64-bit or key is longer than 128-bit.
     Raises error if text or key is not a number.
     """
     n = kwargs.setdefault('n', 32)
-    if type(text) is not int:
-        raise ValueError("Ciphertext must be a number")
-    if type(key) is not int:
-        raise ValueError("Key must be a number")
-    if text >= 2 ** 64:
+    if len(text) != 64:
         raise ValueError("Ciphertext must be 64-bit")
-    if key >= 2 ** 128:
+    if len(key) != 128:
         raise ValueError("Key must be 128-bit")
     sk = key_schedule(key, n)
-    rn, ln = split(2, 32, _decrypt_preprocessing(sk[n + 4:n + 8], text))
+    preproc = _decrypt_preprocessing(sk[n + 4:n + 8], text)
+    rn, ln = preproc[:32], preproc[32:]
     l, r = _decrypt_iterative_calculation(ln, rn, sk, n)
     l0, r0 = l[0], r[0]
-    p = concat_bits(l0, r0, n=32) ^ l0
-    p ^= concat_bits(sk[n], sk[n + 1], sk[n + 2], sk[n + 3], n=16)
+    p = (l0 + r0) ^ bitseq64(l0)
+    p ^= sum([sk[n], sk[n + 1], sk[n + 2], sk[n + 3]])
     return p
 
 
