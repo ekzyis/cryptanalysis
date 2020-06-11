89aca3e0e29efba5d34a02a82be6889ff2d1dc6b
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-11 23:26:10 +0200

    Big refactor and added more helper assertion'ers

--- test/helper.py
+++ test/helper.py
@@ -39,33 +39,60 @@ class BitsTestCase(unittest.TestCase):
             self.assertEqual(b.uint, value)
             self.assertEqual(len(b), length)
 
+        def _assert_fn_raises_with_mutated_argument_length_wrapper(arg_len_mutator):
+            def _assert_fn_raises_with_mutated_argument_length(fn, correct_args, arg_index_to_check, error):
+                length = len(correct_args[arg_index_to_check])
+                with self.assertRaises(error):
+                    x = bitseq(0x0, bit=arg_len_mutator(length))
+                    args = correct_args.copy()
+                    args[arg_index_to_check] = x
+                    fn(*args)
+
+            return _assert_fn_raises_with_mutated_argument_length
+
+        _assert_fn_raises_if_argument_smaller_than_given_length = \
+            _assert_fn_raises_with_mutated_argument_length_wrapper(lambda l: l - 1)
+
+        _assert_fn_raises_if_argument_greater_than_given_length = \
+            _assert_fn_raises_with_mutated_argument_length_wrapper(lambda l: l + 1)
+
         def _assert_fn_raises_if_argument_not_of_given_length(fn, correct_args, arg_index_to_check, error):
-            length = len(correct_args[arg_index_to_check])
-            with self.assertRaises(error):
-                x = bitseq(0x0, bit=length + 1)
-                args = correct_args.copy()
-                args[arg_index_to_check] = x
-                fn(*args)
-            with self.assertRaises(error):
-                x = bitseq(0x0, bit=length - 1)
-                args = correct_args.copy()
-                args[arg_index_to_check] = x
-                fn(*args)
+            _assert_fn_raises_if_argument_greater_than_given_length(fn, correct_args, arg_index_to_check, error)
+            _assert_fn_raises_if_argument_smaller_than_given_length(fn, correct_args, arg_index_to_check, error)
             try:
                 fn(*correct_args)
             except ValueError:
                 self.fail("{} raised unexpected ValueError".format(fn.__name__))
 
-        def assert_fn_raises_if_arguments_not_of_given_lengths(fn, correct_args, error, arg_index_to_check=None):
-            if arg_index_to_check is None:
-                for i, arg in enumerate(correct_args):
-                    _assert_fn_raises_if_argument_not_of_given_length(
-                        fn=fn, correct_args=correct_args, arg_index_to_check=i, error=error
+        def _assert_fn_arguments_length_iterator_wrapper(assert_argument_length_fn):
+            def _assert_fn_raises_if_assert_fn_called_on_arg_length(fn, correct_args, error, arg_index_to_check=None):
+                if arg_index_to_check is None:
+                    for i, arg in enumerate(correct_args):
+                        assert_argument_length_fn(
+                            fn=fn, correct_args=correct_args, arg_index_to_check=i, error=error
+                        )
+                else:
+                    assert_argument_length_fn(
+                        fn=fn, correct_args=correct_args, arg_index_to_check=arg_index_to_check, error=error
                     )
-            else:
-                _assert_fn_raises_if_argument_not_of_given_length(
-                    fn=fn, correct_args=correct_args, arg_index_to_check=arg_index_to_check, error=error
-                )
+
+            return _assert_fn_raises_if_assert_fn_called_on_arg_length
+
+        assert_fn_raises_if_arguments_not_of_given_lengths = _assert_fn_arguments_length_iterator_wrapper(
+            _assert_fn_raises_if_argument_not_of_given_length
+        )
+
+        assert_fn_raises_if_arguments_smaller_than_given_lengths = _assert_fn_arguments_length_iterator_wrapper(
+            _assert_fn_raises_if_argument_smaller_than_given_length
+        )
+
+        assert_fn_raises_if_arguments_greater_than_given_lengths = _assert_fn_arguments_length_iterator_wrapper(
+            _assert_fn_raises_if_argument_greater_than_given_length
+        )
 
         self.assertBit = assert_bit
         self.assert_fn_raises_if_arguments_not_of_given_lengths = assert_fn_raises_if_arguments_not_of_given_lengths
+        self.assert_fn_raises_if_arguments_smaller_than_given_lengths = \
+            assert_fn_raises_if_arguments_smaller_than_given_lengths
+        self.assert_fn_raises_if_arguments_greater_than_given_lengths = \
+            assert_fn_raises_if_arguments_greater_than_given_lengths
