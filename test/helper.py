813c100478ecdde092b1cc5e3a1e03e661244294
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-11 22:40:45 +0200

    Created helper assertion functions for argument length test

--- test/helper.py
+++ test/helper.py
@@ -8,6 +8,8 @@ import unittest
 # https://medium.com/pythonhive/python-decorator-to-measure-the-execution-time-of-methods-fa04cb6bb36d
 import bitstring
 
+from util.bitseq import bitseq
+
 
 def timeit(method):
     """Decorator to print execution time of method."""
@@ -37,4 +39,28 @@ class BitsTestCase(unittest.TestCase):
             self.assertEqual(b.uint, value)
             self.assertEqual(len(b), length)
 
+        def assert_fn_raises_if_argument_not_of_given_length(fn, correct_args, arg_index_to_check, length, error):
+            with self.assertRaises(error):
+                x = bitseq(0x0, bit=length + 1)
+                args = correct_args.copy()
+                args[arg_index_to_check] = x
+                fn(*args)
+            with self.assertRaises(error):
+                x = bitseq(0x0, bit=length - 1)
+                args = correct_args.copy()
+                args[arg_index_to_check] = x
+                fn(*args)
+            try:
+                fn(*correct_args)
+            except ValueError:
+                self.fail("{} raised unexpected ValueError".format(fn.__name__))
+
+        def assert_fn_raises_if_arguments_not_of_given_lengths(fn, correct_args, error):
+            for i, arg in enumerate(correct_args):
+                assert_fn_raises_if_argument_not_of_given_length(
+                    fn=fn, correct_args=correct_args, arg_index_to_check=i, length=len(arg), error=error
+                )
+
         self.assertBit = assert_bit
+        self.assert_fn_raises_if_argument_not_of_given_length = assert_fn_raises_if_argument_not_of_given_length
+        self.assert_fn_raises_if_arguments_not_of_given_lengths = assert_fn_raises_if_arguments_not_of_given_lengths
