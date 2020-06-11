e8da4393c81c1239839c0a530882806f6b29cf18
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-11 15:27:21 +0200

    \n before and after time output

--- test/helper.py
+++ test/helper.py
@@ -17,7 +17,7 @@ def timeit(method):
             name = kw.get('log_name', method.__name__.upper())
             kw['log_time'][name] = int((te - ts) * 1000)
         else:
-            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+            print('\n%r  %2.2f ms\n' % (method.__name__, (te - ts) * 1000))
         return result
 
     return timed
