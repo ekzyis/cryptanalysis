193cd0cd7db9ed95867c7ac0e3d0433290902082
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-11 14:46:12 +0200

    Monkey patch unittest.TestCase to show execution time

--- test/context.py
+++ test/context.py
@@ -6,7 +6,24 @@ importing them in test modules.
 """
 
 import sys
+import unittest
 from pathlib import Path
+from time import time
 
 src_dir = Path(__file__).parent / '../src'
 sys.path.insert(0, str(src_dir))
+
+
+# monkey patch unittest.TestCase such that it shows execution time of all test methods
+@classmethod
+def setUpClass(cls):
+    cls.startTime = time()
+
+
+@classmethod
+def tearDownClass(cls):
+    print("\n%s.%s: %.3fs" % (cls.__module__, cls.__name__, time() - cls.startTime))
+
+
+unittest.TestCase.setUpClass = setUpClass
+unittest.TestCase.tearDownClass = tearDownClass
