0131a867839451c45fb4654b19f20c174bec2799
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-06-13 14:14:17 +0200

    test_chacha20_encrypt: PENDING

---
+++ test/ciphers/chacha20/patchers.py
@@ -0,0 +1,12 @@
+"""Decorators used for ChaCha20 tests."""
+from unittest import mock
+
+
+def iv_patch(*args, **kwargs):
+    """Patch for Initialization vector."""
+    return mock.patch('random.randrange', *args, **kwargs)
+
+
+def initial_counter(value):
+    """Patch for initial counter."""
+    return mock.patch('src.ciphers.stream.chacha20.initial_counter', return_value=value)
